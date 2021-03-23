edb1261f5308b8a5365429e76f70afd47ba4b5b9
Peter_Wang_Alienware <peterwang724@gmail.com>
2021-03-23 16:42:04 -0700

    cleaned up analysis

--- deg_test.py
+++ deg_test.py
@@ -15,15 +15,18 @@ import shutil
 from moviepy.editor import VideoFileClip, concatenate_videoclips, clips_array
 import pprint
 
-
-deg_directory = r'C:\Users\Peter\Desktop\DEG\reach_merged_v0_deepethogram'
+input_folder = r'C:\Users\Peter_Wang_Alienware\Desktop\DATA\M9\2021.03.09'
+deg_directory = r'C:\Users\Peter_Wang_Alienware\Desktop\DEG\reach_merged_v0_deepethogram'
 data_directory = os.path.join(deg_directory, 'DATA')
 config_path = os.path.join(deg_directory, 'project_config.yaml')
 cfg = projects.load_config(config_path)
 
-BATCH_SIZE = 16 # small but not too small
+BATCH_SIZE = 8 # small but not too small
 STEPS_PER_EPOCH = 500 # if less than 10, might have bugs with visualization
 NUM_EPOCHS = 500
+
+train = True
+analyze = False
 
 def command_from_string(string):
     command = string.split(' ')
@@ -43,7 +46,6 @@ def add_default_arguments(string, train=True):
 
 
 # # PREPROCESSING
-# input_folder = r'C:\Users\Peter\Desktop\DATA\M5\2021_03_11'
 # videos0 = [os.path.join(input_folder, x) for x in os.listdir(input_folder)
 #            if 'CAM0' in x]
 # videos0 = sorted(videos0)
@@ -63,10 +65,13 @@ def add_default_arguments(string, train=True):
 #         clip2 = VideoFileClip(v1)
 #         final_clip = clips_array([[clip1, clip2]])
 #         final_clip.write_videofile(pn, codec='libx264')
-#
+
+# Add videos to project
 # videos = [os.path.join(merged_folder, x) for x in os.listdir(merged_folder) if
 #           '.mp4' in x]
-# # Add videos to project
+videos = [os.path.join(input_folder, x) for x in os.listdir(input_folder) if
+          'CAM0.avi' in x]
+
 # for video in videos:
 #     basename = os.path.basename(video)
 #     vidname = os.path.splitext(basename)[0]
@@ -75,108 +80,109 @@ def add_default_arguments(string, train=True):
 #     except:
 #         print(f'{vidname} already exists')
 
-# TRAINING
+if train:
+    # pretrained weights
+    pretrained_folder = os.path.join(deg_directory, 'models', 'pretrained')
+    pretrained_flow_weights = os.path.join(
+        pretrained_folder,
+        '200221_115158_TinyMotionNet',
+        'checkpoint.pt')
 
-# pretrained weights
-pretrained_folder = r'C:\Users\Peter\Desktop\DEG\mouse_reach_deepethogram\models\pretrained'
-pretrained_flow_weights = os.path.join(
-    pretrained_folder,
-    '200221_115158_TinyMotionNet',
-    'checkpoint.pt')
+    pretrained_feature_weights = os.path.join(
+        pretrained_folder,
+        '200415_125824_hidden_two_stream_kinetics_degf',
+        'checkpoint.pt')
 
-pretrained_feature_weights = os.path.join(
-    pretrained_folder,
-    '200415_125824_hidden_two_stream_kinetics_degf',
-    'checkpoint.pt')
+    # # train flow generation
+    # string = (f'python -m deepethogram.flow_generator.train '
+    #           f'preset=deg_f '
+    #           f'flow_generator.weights={pretrained_flow_weights} ')
+    # string = add_default_arguments(string)
+    # command = command_from_string(string)
+    # ret = subprocess.run(command)
 
-# train flow generation
-string = (f'python -m deepethogram.flow_generator.train '
-          f'preset=deg_f '
-          f'flow_generator.weights={pretrained_flow_weights} ')
-string = add_default_arguments(string)
-command = command_from_string(string)
-ret = subprocess.run(command)
+    # train feature extraction
+    string = (
+        f'python -m deepethogram.feature_extractor.train '
+        f'preset=deg_f '
+        f'flow_generator.weights=latest '
+        f'feature_extractor.weights={pretrained_feature_weights} ')
+    string = add_default_arguments(string)
+    command = command_from_string(string)
+    ret = subprocess.run(command)
 
-# train feature extraction
-string = (
-    f'python -m deepethogram.feature_extractor.train '
-    f'preset=deg_f '
-    f'flow_generator.weights=latest '
-    f'feature_extractor.weights={pretrained_feature_weights} ')
-string = add_default_arguments(string)
-command = command_from_string(string)
-ret = subprocess.run(command)
+    # train sequence
+    string = (f'python -m deepethogram.sequence.train ')
+    string = add_default_arguments(string)
+    command = command_from_string(string)
+    print(command)
+    ret = subprocess.run(command)
 
-# train sequence
-string = (f'python -m deepethogram.sequence.train ')
-string = add_default_arguments(string)
-command = command_from_string(string)
-print(command)
-ret = subprocess.run(command)
+if analyze:
+    subdirs = utils.get_subfiles(data_directory, 'directory')
+    dir_string = ','.join([str(i) for i in subdirs])
+    dir_string = '[' + dir_string + ']'
 
-# # # ANALYSIS
-# subdirs = utils.get_subfiles(data_directory, 'directory')
-# dir_string = ','.join([str(i) for i in subdirs])
-# dir_string = '[' + dir_string + ']'
-#
-# # feature extraction inference
-# string = (f'python -m deepethogram.feature_extractor.inference preset=deg_f reload.latest=True ')
-# string += f'inference.directory_list={dir_string} inference.overwrite=True '
-# string = add_default_arguments(string, train=False)
-# command = command_from_string(string)
-# ret = subprocess.run(command)
-#
-# # sequential model extraction
-# string = (f'python -m deepethogram.sequence.inference preset=deg_f reload.latest=True ')
-# string += f'inference.directory_list={dir_string} inference.overwrite=True '
-# string = add_default_arguments(string, train=False)
-# command = command_from_string(string)
-# ret = subprocess.run(command)
-#
-# # set predictions as labels with slight modification for grab probs.
-# np.set_printoptions(suppress=True)
-# for video in videos:
-#     vidname = os.path.basename(video)
-#     basename = os.path.splitext(vidname)[0]
-#     outputfile = os.path.join(data_directory,
-#                               basename,
-#                               basename + '_outputs.h5')
-#     outputs = projects.import_outputfile(cfg['project']['path'],
-#                                          outputfile,
-#                                          class_names=cfg['project']['class_names'],
-#                                          latent_name=None)
-#
-#     probabilities, thresholds, latent_name, keys = outputs
-#     grab_ix = cfg['project']['class_names'].index('grab')
-#     thresholds[grab_ix] = 0.95
-#     postprocessor = postpro.MinBoutLengthPostprocessor(thresholds,
-#                                                        bout_length=2)
-#     estimated_labels = postprocessor(probabilities)
-#
-#     df = pd.DataFrame(data=estimated_labels,
-#                       columns=cfg['project']['class_names'])
-#     prediction_fname = os.path.join(data_directory,
-#                                     basename,
-#                                     basename + '_labels.csv')
-#     df.to_csv(prediction_fname)
-#     print(df.sum(axis=0))
-#     print(prediction_fname)
-# #
-# # FINAL
-# save_dir = os.path.join(input_folder, 'ANALYZED', 'LABELS')
-# os.makedirs(save_dir, exist_ok=True)
-#
-# # move analyzed data to the local data directory and delete the avi files
-# for video in videos:
-#     vidname = os.path.basename(video)
-#     basename = os.path.splitext(vidname)[0]
-#     folder = os.path.join(data_directory, basename)
-#     save_folder = os.path.join(save_dir, basename)
-#     shutil.copytree(folder, save_folder)
-#     print(f'moved to {save_folder}')
-# all_avis = tools.get_files(save_dir, ('.avi'))
-# for avi in all_avis:
-#     os.remove(avi)
+    # feature extraction inference
+    string = (f'python -m deepethogram.feature_extractor.inference preset=deg_f reload.latest=True ')
+    string += f'inference.directory_list={dir_string} inference.overwrite=True '
+    string = add_default_arguments(string, train=False)
+    command = command_from_string(string)
+    ret = subprocess.run(command)
+
+    # sequential model extraction
+    string = (f'python -m deepethogram.sequence.inference preset=deg_f reload.latest=True ')
+    string += f'inference.directory_list={dir_string} inference.overwrite=True '
+    string = add_default_arguments(string, train=False)
+    command = command_from_string(string)
+    ret = subprocess.run(command)
+
+    # set predictions as labels with slight modification for grab probs.
+    np.set_printoptions(suppress=True)
+    for video in videos:
+        vidname = os.path.basename(video)
+        basename = os.path.splitext(vidname)[0]
+        outputfile = os.path.join(data_directory,
+                                  basename,
+                                  basename + '_outputs.h5')
+        outputs = projects.import_outputfile(cfg['project']['path'],
+                                             outputfile,
+                                             class_names=cfg['project']['class_names'],
+                                             latent_name=None)
+
+        probabilities, thresholds, latent_name, keys = outputs
+        grab_ix = cfg['project']['class_names'].index('grab')
+        thresholds[grab_ix] = 0.95
+        postprocessor = postpro.MinBoutLengthPostprocessor(thresholds,
+                                                           bout_length=2)
+        estimated_labels = postprocessor(probabilities)
+
+        df = pd.DataFrame(data=estimated_labels,
+                          columns=cfg['project']['class_names'])
+        prediction_fname = os.path.join(data_directory,
+                                        basename,
+                                        basename + '_labels.csv')
+        df.to_csv(prediction_fname)
+        print(df.sum(axis=0))
+        print(prediction_fname)
+
+        save_dir = os.path.join(input_folder, 'ANALYZED', 'LABELS')
+        os.makedirs(save_dir, exist_ok=True)
+
+    # move analyzed data to the local data directory and delete the avi files
+    for video in videos:
+        vidname = os.path.basename(video)
+        basename = os.path.splitext(vidname)[0]
+        folder = os.path.join(data_directory, basename)
+        save_folder = os.path.join(save_dir, basename)
+        try:
+            shutil.copytree(folder, save_folder)
+            print(f'moved to {save_folder}')
+        except:
+            print('exists: {}'.format(save_folder))
+    all_avis = tools.get_files(save_dir, ('.avi'))
+    for avi in all_avis:
+        os.remove(avi)
 
 
 
